d4ad16d3b67e30c37ecc144e40b10eaa1b54184a
antialopez <antialopezgarcia@gmail.com>
2023-04-19 17:24:18 +0200

    Implementation request with Retrofit and CoroutineScope

--- app/src/main/java/com/example/superheroapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.superheroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.widget.SearchView
 import com.example.superheroapp.databinding.ActivityMainBinding
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -18,26 +23,34 @@ class MainActivity : AppCompatActivity() {
         initUnit()
     }
 
-    private fun initUnit(){
+    private fun initUnit() {
 
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener
-        {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             //Busca al pulsar boton
             override fun onQueryTextSubmit(query: String?): Boolean {
                 searchByName(query.orEmpty())
                 return false
             }
+
             //Busca a medida que vamos escribiendo
             override fun onQueryTextChange(newText: String?): Boolean {
-            return false
+                return false
             }
         })
     }
-    private fun searchByName(query: String){
 
+    private fun searchByName(query: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val myResponse = retrofit.create(ApiService::class.java).getSuperheroes(query)
+            if(myResponse.isSuccessful){
+                Log.i("superheroes","funciona")
+            }else{
+                Log.i("superheroes","no funciona")
+            }
+        }
     }
 
-    private fun getRetrofit():Retrofit{
+    private fun getRetrofit(): Retrofit {
         return Retrofit
             .Builder()
             .baseUrl("https://www.superheroapi.com/")
