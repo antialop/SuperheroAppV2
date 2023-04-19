f85a55608f925501e9fa3376713dc79e6a1bbec6
antialopez <antialopezgarcia@gmail.com>
2023-04-19 16:32:19 +0200

    viewBinding

--- app/src/main/java/com/example/superheroapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.superheroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
 import com.example.superheroapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+    }
+
+    private fun initUnit(){
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener
+        {
+            //Busca al pulsar boton
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchByName(query.orEmpty())
+                return false
+            }
+            //Busca a medida que vamos escribiendo
+            override fun onQueryTextChange(newText: String?): Boolean {
+            return false
+            }
+        })
+    }
+    private fun searchByName(query: String){
 
     }
 }
