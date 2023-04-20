e24467d2eb878b8730a1d3cc5d153109d0f8641d
antialopez <antialopezgarcia@gmail.com>
2023-04-20 15:27:58 +0200

    crate coroutine for details activity

--- app/src/main/java/com/example/superheroapp/DetailSuperheroActivity.kt
+++ app/src/main/java/com/example/superheroapp/DetailSuperheroActivity.kt
@@ -2,6 +2,11 @@ package com.example.superheroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class DetailSuperheroActivity : AppCompatActivity() {
     companion object{
@@ -10,5 +15,21 @@ class DetailSuperheroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_superhero)
+        //Recuperar id, si el valor es null vacio
+        val id : String = intent.getStringExtra(EXTRA_ID).orEmpty()
+        getSuperHeroInformation(id)
+    }
+
+    private fun getSuperHeroInformation(id: String) {
+        CoroutineScope(Dispatchers.IO).launch{
+            getRetrofit().create(ApiService::class.java)
+        }
+    }
+    private fun getRetrofit(): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl("https://superheroapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
