48ff5611085c31346483a044c608f1735dcae7cd
antialopez <antialopezgarcia@gmail.com>
2023-04-20 11:41:14 +0200

    create Recyclerview. Adapter and ViewHolder

---
+++ app/src/main/java/com/example/superheroapp/SuperheroAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.superheroapp
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SuperheroAdapter (var superheroList: List<SuperheroItemResponse> = emptyList()) :
+    RecyclerView.Adapter<SuperheroViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperheroViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SuperheroViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
