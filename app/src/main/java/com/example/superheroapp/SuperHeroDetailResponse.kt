4b6fff0759ecb13720d5bde0ff3ff16b20dfb006
antialopez <antialopezgarcia@gmail.com>
2023-04-20 16:57:13 +0200

    biography response

--- app/src/main/java/com/example/superheroapp/SuperHeroDetailResponse.kt
+++ app/src/main/java/com/example/superheroapp/SuperHeroDetailResponse.kt
@@ -5,7 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class SuperHeroDetailResponse(
     @SerializedName("name") val name: String,
     @SerializedName("powerstats") val powerstats: PowerStatsResponse,
-    @SerializedName("image") val image: SuperheroImageDetailResponse
+    @SerializedName("image") val image: SuperheroImageDetailResponse,
+    @SerializedName("biography") val biography: Biography
 )
 
 
@@ -19,3 +20,8 @@ data class PowerStatsResponse(
 )
 
 data class SuperheroImageDetailResponse(@SerializedName("url") val url:String )
+
+data class Biography(
+    @SerializedName("full-name") val fullName:String,
+    @SerializedName("publisher") val publisher:String
+    )
