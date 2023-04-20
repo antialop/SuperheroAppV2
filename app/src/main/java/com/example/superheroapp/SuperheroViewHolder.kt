57d65d235888374f0c73dcccd97b6039e9ee63c5
antialopez <antialopezgarcia@gmail.com>
2023-04-20 13:07:57 +0200

    call url image

--- app/src/main/java/com/example/superheroapp/SuperheroViewHolder.kt
+++ app/src/main/java/com/example/superheroapp/SuperheroViewHolder.kt
@@ -3,10 +3,12 @@ package com.example.superheroapp
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.superheroapp.databinding.ItemSuperheroBinding
+import com.squareup.picasso.Picasso
 
 class SuperheroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val binding = ItemSuperheroBinding.bind(view)
     fun bind(superheroItemResponse: SuperheroItemResponse) {
         binding.tvSuperheroName.text = superheroItemResponse.name
+        Picasso.get().load(superheroItemResponse.superheroImage.url).into(binding.ivSuperhero)
     }
 }
