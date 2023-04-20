bfcae58f20ac1fe05e91e5dd43a9f2b750ce9973
antialopez <antialopezgarcia@gmail.com>
2023-04-20 13:48:14 +0200

    update superherolist with onItemSelected

--- app/src/main/java/com/example/superheroapp/SuperheroAdapter.kt
+++ app/src/main/java/com/example/superheroapp/SuperheroAdapter.kt
@@ -4,21 +4,30 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class SuperheroAdapter (var superheroList: List<SuperheroItemResponse> = emptyList()) :
+//EL adaptador recibe un callBack, las id estan dentro del viewholder, variable que llame a un codigo y le pase un String
+//El metodo navigateToResult es como si estuviero dentro de la variable onItenSelected
+//Llamar a onItemSelected es commo se llamaras a navigateToResult
+class SuperheroAdapter(
+    var superheroList: List<SuperheroItemResponse> = emptyList(),
+    private val onItemSelected: (String) -> Unit
+) :
     RecyclerView.Adapter<SuperheroViewHolder>() {
 
-    fun updateList(superheroList: List<SuperheroItemResponse>){
+    fun updateList(superheroList: List<SuperheroItemResponse>) {
         this.superheroList = superheroList
         notifyDataSetChanged()
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperheroViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        return SuperheroViewHolder(layoutInflater.inflate(R.layout.item_superhero,parent,false))
+        return SuperheroViewHolder(layoutInflater.inflate(R.layout.item_superhero, parent, false))
     }
 
     override fun getItemCount() = superheroList.size
 
     override fun onBindViewHolder(viewholder: SuperheroViewHolder, position: Int) {
-        viewholder.bind(superheroList[position])
+        //Ademas de pasar el objeto superheroe le pasamos el onItemSelected
+        // y modificamos la funcion bind (viewHolder) pasandole la lambda onselectedIntem
+        viewholder.bind(superheroList[position],onItemSelected)
     }
 }
--- app/src/main/java/com/example/superheroapp/SuperheroViewHolder.kt
+++ app/src/main/java/com/example/superheroapp/SuperheroViewHolder.kt
@@ -7,8 +7,13 @@ import com.squareup.picasso.Picasso
 
 class SuperheroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val binding = ItemSuperheroBinding.bind(view)
-    fun bind(superheroItemResponse: SuperheroItemResponse) {
+    //Cada bind es uno de los items
+    fun bind(superheroItemResponse: SuperheroItemResponse,onItemSelected: (String) -> Unit) {
         binding.tvSuperheroName.text = superheroItemResponse.name
         Picasso.get().load(superheroItemResponse.superheroImage.url).into(binding.ivSuperhero)
+        binding.root.setOnClickListener {
+            //Cada vez que se pulse la lista le pasamos el id
+            onItemSelected(superheroItemResponse.superheroId)
+        }
     }
 }
