9bace81b263b3f0768adc0f597108a867174e941
antialopez <antialopezgarcia@gmail.com>
2023-04-20 12:08:11 +0200

    create function updateList

--- app/src/main/java/com/example/superheroapp/SuperheroAdapter.kt
+++ app/src/main/java/com/example/superheroapp/SuperheroAdapter.kt
@@ -6,6 +6,11 @@ import androidx.recyclerview.widget.RecyclerView
 
 class SuperheroAdapter (var superheroList: List<SuperheroItemResponse> = emptyList()) :
     RecyclerView.Adapter<SuperheroViewHolder>() {
+
+    fun updateList(superheroList: List<SuperheroItemResponse>){
+        this.superheroList = superheroList
+        notifyDataSetChanged()
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperheroViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return SuperheroViewHolder(layoutInflater.inflate(R.layout.item_superhero,parent,false))
