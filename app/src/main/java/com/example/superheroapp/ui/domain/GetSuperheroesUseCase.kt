81c1fb02944e433795ee1f1bfd20899443833863
antialopez <antialopezgarcia@gmail.com>
2023-04-26 16:16:26 +0200

    superheroes with clean architecture and MVVM

--- app/src/main/java/com/example/superheroapp/ui/domain/GetSuperheroesUseCase.kt
+++ app/src/main/java/com/example/superheroapp/ui/domain/GetSuperheroesUseCase.kt
@@ -4,6 +4,10 @@ import com.example.superheroapp.data.SuperheroRepository
 
 class GetSuperheroesUseCase {
     private val repository = SuperheroRepository()
-    suspend operator fun invoke(superheroName: String) = repository.getAllSuperheroes(superheroName)
+    //En vez de query he cambiado el nombre a superheroName ya que es el nombre con el que voy a buscar
+    //El invoke es para cuando llames a la clase ya se ejecuta la funcion
+    //Este metodo devuelve la funcion del repositorio getAllSuperheroes que devuelve los superheroes por nombre
+    // que le pasamos el name, que es la query
+    suspend operator fun invoke(superheroName: String) = repository.getSuperheroByName(superheroName)
 
 }
