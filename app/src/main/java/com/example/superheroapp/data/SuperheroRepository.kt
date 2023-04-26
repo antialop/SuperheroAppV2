31d222b1bbd80c77d5db2a855218a148445bdc3e
antialopez <antialopezgarcia@gmail.com>
2023-04-26 18:06:27 +0200

    inject

--- app/src/main/java/com/example/superheroapp/data/SuperheroRepository.kt
+++ app/src/main/java/com/example/superheroapp/data/SuperheroRepository.kt
@@ -4,10 +4,11 @@ import com.example.superheroapp.data.network.SuperheroService
 import com.example.superheroapp.ui.domain.SuperHero
 import com.example.superheroapp.ui.domain.SuperHeroDetail
 import com.example.superheroapp.ui.domain.toDomain
+import javax.inject.Inject
 
-class SuperheroRepository {
-    private val api = SuperheroService()
-
+class SuperheroRepository @Inject  constructor(
+    private val api:SuperheroService
+){
     //Esta funcion me devuelve un SuperHero, que es el modelo que sigue la ui
     suspend fun getSuperheroByName(superheroName: String) : SuperHero {
         //Pero la api sigue el modelo SuperheroDataResponse por eso le hago toDomain()
--- app/src/main/java/com/example/superheroapp/ui/domain/GetSuperheroesUseCase.kt
+++ app/src/main/java/com/example/superheroapp/ui/domain/GetSuperheroesUseCase.kt
@@ -1,9 +1,11 @@
 package com.example.superheroapp.ui.domain
 
 import com.example.superheroapp.data.SuperheroRepository
+import javax.inject.Inject
 
-class GetSuperheroesUseCase {
-    private val repository = SuperheroRepository()
+class GetSuperheroesUseCase @Inject constructor(
+    private val repository: SuperheroRepository
+){
     //En vez de query he cambiado el nombre a superheroName ya que es el nombre con el que voy a buscar
     //El invoke es para cuando llames a la clase ya se ejecuta la funcion
     //Este metodo devuelve la funcion del repositorio getAllSuperheroes que devuelve los superheroes por nombre
