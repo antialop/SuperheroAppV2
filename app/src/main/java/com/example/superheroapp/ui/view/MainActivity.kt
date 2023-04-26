96a88d0f0a6902e550e2592f63e87b9c008350a2
antialopez <antialopezgarcia@gmail.com>
2023-04-26 18:48:03 +0200

    add progressbar

--- app/src/main/java/com/example/superheroapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/superheroapp/ui/view/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.superheroapp.databinding.ActivityMainBinding
@@ -28,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         superheroViewModel.superHeroDataResponse.observe(this, Observer {
             adapter.updateList(it)
+            binding.progressBar.isVisible = false
         })
     }
 
@@ -37,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             //Busca al pulsar boton
             override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.progressBar.isVisible = true
                 superheroViewModel.searchByName(query.orEmpty())
                 return false
             }
